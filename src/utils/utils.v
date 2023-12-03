c4896b753b8819dfd79e19269446f7436304a039
greidinger-reis <jp.greidinger@hotmail.com>
2023-12-03 16:24:32 -0300

    refactored get_input and get_actual_input

--- src/utils/utils.v
+++ src/utils/utils.v
@@ -15,9 +15,18 @@ pub fn Rune.to_int(r rune) ?int {
 }
 
 pub fn get_actual_input(file_path string) string {
-	return os.read_file(file_path.replace('.v', '.txt')) or { panic(err) }
+	mut path := file_path.split('/')
+	path.pop()
+	path.pop()
+	path << 'input'
+
+	return os.read_file(path.join('/')) or { panic(err) }
 }
 
 pub fn get_example_input(file_path string) string {
-	return os.read_file(file_path.replace('.v', '.example.txt')) or { panic(err) }
+	mut path := file_path.split('/')
+	path.pop()
+	path << 'example_input'
+
+	return os.read_file(path.join('/')) or { panic(err) }
 }
