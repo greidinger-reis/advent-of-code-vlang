1698ced91ebdf008cf6d982e66d93a4bc065d6fd
greidinger-reis <jp.greidinger@hotmail.com>
2023-12-03 16:23:56 -0300

    day_2

--- src/year_2023/day_2/part_1/part_1.v
+++ src/year_2023/day_2/part_1/part_1.v
@@ -6,75 +6,130 @@ import arrays
 const file = @FILE
 pub const example_input = utils.get_example_input(file)
 pub const actual_input = utils.get_actual_input(file)
-const cube_names = ['red', 'blue', 'green']
+const cube_names = ['red', 'green', 'blue']
 
-struct Game {
-	id               string
-	red_cube_count   int
-	blue_cube_count  int
-	green_cube_count int
+enum Cube {
+	red
+	green
+	blue
 }
 
-fn Game.parse(line string) &Game {
-	parts := line.split(':')
-	game_id := parts[0].replace('Game ', '')
-	mut red_cube_count := 0
-	mut green_cube_count := 0
-	mut blue_cube_count := 0
+struct Game {
+	id    int
+	draws []Draw
+}
 
-	for i := 1; i < parts[1].len; i++ {
-		sub_str := parts[1][i..parts[1].len]
-		n0 := sub_str[0].str().int()
-		n1 := sub_str[1].str().int()
-		mut cube := ''
+struct Draw {
+	cubes []DrawCube
+}
 
-		if n0 != 0 {
-			if n1 != 0 {
-				for c in part_1.cube_names {
-					if sub_str[3..sub_str.len].contains(c) {
-						cube = c
-						break
+struct DrawCube {
+	cube  Cube
+	count int
+}
+
+struct PossibleGamesArgs {
+	red   int
+	green int
+	blue  int
+}
+
+// find all games that are possible with the given args
+// args define the maximum possible number of cubes of each color in each draw
+// if a draw contains more cubes than the maximum, the game is not possible
+fn (games []Game) possible_games(args PossibleGamesArgs) []Game {
+	mut possible_games := []Game{cap: games.len}
+
+	for game in games {
+		mut possible := true
+
+		for draw in game.draws {
+			for cubes in draw.cubes {
+				match cubes.cube {
+					.red {
+						if cubes.count > args.red {
+							possible = false
+							break
+						}
+					}
+					.green {
+						if cubes.count > args.green {
+							possible = false
+							break
+						}
+					}
+					.blue {
+						if cubes.count > args.blue {
+							possible = false
+							break
+						}
 					}
 				}
 			}
-
-			for c in part_1.cube_names {
-				if sub_str[2..sub_str.len].contains(c) {
-					cube = c
-					break
-				}
+			if !possible {
+				break
 			}
+		}
 
-            count := if n1 != 0 { '${n0}${n1}'.int() } else { n0 }
-            println('Cube: ${cube}, Count: ${count}')
-
-            match cube {
-                'red' {
-                    red_cube_count += count
-                }
-                'blue' {
-                    blue_cube_count += count
-                }
-                'green' {
-                    green_cube_count += count
-                }
-                else {
-                    panic('Unknown cube color: ${cube}')
-                }
-            }
+		if possible {
+			possible_games << game
 		}
 	}
 
-	return &Game{
+	return possible_games
+}
+
+fn Game.from_line(line string) Game {
+	parts := line.split(':')
+	game_id := parts[0].replace('Game ', '').int()
+
+	raw_draws := parts[1].split(';')
+	draws := raw_draws.map(parse_draw(it))
+
+	return Game{
 		id: game_id
-		red_cube_count: red_cube_count
-		green_cube_count: green_cube_count
-		blue_cube_count: blue_cube_count
+		draws: draws
+	}
+}
+
+fn parse_draw(draw_str string) Draw {
+	trimmed_draw := draw_str.trim_space()
+	cube_counts_raw := trimmed_draw.split(', ')
+
+	draw_cubes := cube_counts_raw.map(fn (it string) DrawCube {
+		cube_count_parts := it.split(' ')
+		count := cube_count_parts[0].int()
+		cube := cube_from_str(cube_count_parts[1])
+		return DrawCube{
+			cube: cube
+			count: count
+		}
+	})
+
+	return Draw{
+		cubes: draw_cubes
+	}
+}
+
+fn cube_from_str(str string) Cube {
+	return match str {
+		'red' {
+			.red
+		}
+		'green' {
+			.green
+		}
+		'blue' {
+			.blue
+		}
+		else {
+			panic('Unknown cube color: ${str}')
+		}
 	}
 }
 
 pub fn solve(input string) int {
-	games := input.split_into_lines().map(Game.parse(it))
-    dump(games)
-	return 0
+	games := input.split_into_lines().map(Game.from_line(it))
+	possible_games := games.possible_games(red: 12, green: 13, blue: 14)
+	return arrays.sum(possible_games.map(it.id)) or { panic(err) }
 }
