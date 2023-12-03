faa0f052a7b0d32bb4ef1fa3b7777e16b2c87db3
greidinger-reis <jp.greidinger@hotmail.com>
2023-12-03 17:16:04 -0300

    day2_part_2

--- src/utils/utils.v
+++ src/utils/utils.v
@@ -17,16 +17,16 @@ pub fn Rune.to_int(r rune) ?int {
 pub fn get_actual_input(file_path string) string {
 	mut path := file_path.split('/')
 	path.pop()
-	path.pop()
 	path << 'input'
 
 	return os.read_file(path.join('/')) or { panic(err) }
 }
 
-pub fn get_example_input(file_path string) string {
+pub fn get_example_input(file_path string, part int) string {
+	file_name := if part == 1 { 'example_input' } else { 'example_input_2' }
 	mut path := file_path.split('/')
 	path.pop()
-	path << 'example_input'
+	path << file_name
 
 	return os.read_file(path.join('/')) or { panic(err) }
 }
--- src/year_2023/day_2/day_2.v
+++ src/year_2023/day_2/day_2.v
@@ -1,34 +1,38 @@
-module part_1
+module day_2
 
 import utils
 import arrays
 
 const file = @FILE
-pub const example_input = utils.get_example_input(file)
-pub const actual_input = utils.get_actual_input(file)
+pub const input = utils.get_actual_input(file)
+pub const example_input = utils.get_example_input(file, 1)
+pub const example_output_part_1 = 8
+pub const example_output_part_2 = 2286
+
 const cube_names = ['red', 'green', 'blue']
 
-enum Cube {
+pub enum Cube {
 	red
 	green
 	blue
 }
 
-struct Game {
+pub struct Game {
 	id    int
 	draws []Draw
 }
 
-struct Draw {
+pub struct Draw {
 	cubes []DrawCube
 }
 
-struct DrawCube {
+pub struct DrawCube {
 	cube  Cube
 	count int
 }
 
-struct PossibleGamesArgs {
+pub struct CubesCount {
+mut:
 	red   int
 	green int
 	blue  int
@@ -37,7 +41,7 @@ struct PossibleGamesArgs {
 // find all games that are possible with the given args
 // args define the maximum possible number of cubes of each color in each draw
 // if a draw contains more cubes than the maximum, the game is not possible
-fn (games []Game) possible_games(args PossibleGamesArgs) []Game {
+pub fn (games []Game) possible_games(args CubesCount) []Game {
 	mut possible_games := []Game{cap: games.len}
 
 	for game in games {
@@ -79,7 +83,7 @@ fn (games []Game) possible_games(args PossibleGamesArgs) []Game {
 	return possible_games
 }
 
-fn Game.from_line(line string) Game {
+pub fn Game.from_line(line string) Game {
 	parts := line.split(':')
 	game_id := parts[0].replace('Game ', '').int()
 
@@ -128,8 +132,47 @@ fn cube_from_str(str string) Cube {
 	}
 }
 
-pub fn solve(input string) int {
+fn (g &Game) get_min_cube_count() CubesCount {
+	mut min := CubesCount{
+		red: 0
+		green: 0
+		blue: 0
+	}
+
+	for draw in g.draws {
+		for draw_cube in draw.cubes {
+			match draw_cube.cube {
+				.red {
+					if draw_cube.count > min.red {
+						min.red = draw_cube.count
+					}
+				}
+				.green {
+					if draw_cube.count > min.green {
+						min.green = draw_cube.count
+					}
+				}
+				.blue {
+					if draw_cube.count > min.blue {
+						min.blue = draw_cube.count
+					}
+				}
+			}
+		}
+	}
+
+	return min
+}
+
+pub fn part_1(input string) int {
 	games := input.split_into_lines().map(Game.from_line(it))
 	possible_games := games.possible_games(red: 12, green: 13, blue: 14)
 	return arrays.sum(possible_games.map(it.id)) or { panic(err) }
+}
+
+pub fn part_2(input string) int {
+	games := input.split_into_lines().map(Game.from_line(it))
+	min_sets := games.map(it.get_min_cube_count())
+	power_sets := min_sets.map(it.red * it.blue * it.green)
+	return arrays.sum(power_sets) or { panic(err) }
 }
